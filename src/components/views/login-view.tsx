2759d6a7c046dbde702847d48408616b18bf817f
Dennise Catolos <dentolos19@gmail.com>
2024-09-03 12:19:25 +0800

    Fixed login register system

--- src/components/views/login-view.tsx
+++ src/components/views/login-view.tsx
@@ -1,15 +1,22 @@
 import { useAuth } from "@/components/providers/auth-provider";
-import { Box, Button, ButtonGroup, Paper, TextField, Typography } from "@mui/material";
+import { useToast } from "@/components/providers/toast-provider";
+import { Box, Button, Link, Paper, TextField, Typography } from "@mui/material";
 import { useState } from "react";
 
 export default function LoginView() {
   const auth = useAuth();
+  const toast = useToast();
   const [phase, setPhase] = useState<"login" | "register">("login");
 
   const handleLogin = async (data: FormData) => {
     const email = data.get("email") as string;
     const password = data.get("password") as string;
-    await auth.login(email, password);
+    try {
+      await auth.login(email, password);
+    } catch (err) {
+      console.error(err);
+      toast.show("Unable to login! Did you enter the correct credentials?", "error");
+    }
   };
 
   const handleRegister = async (data: FormData) => {
@@ -18,15 +25,20 @@ export default function LoginView() {
     const password = data.get("password") as string;
     const confirmPassword = data.get("confirmPassword") as string;
     if (password !== confirmPassword) return;
-    await auth.register(email, password, name);
+    try {
+      await auth.register(name, email, password);
+    } catch (err) {
+      console.error(err);
+      toast.show("Unable to register! Please try again later.", "error");
+    }
   };
 
   return (
     <Box className={"size-full grid place-items-center"}>
-      <Paper className={"p-8 w-80"} variant={"outlined"}>
+      <Paper className={"p-8 w-96"} variant={"outlined"}>
         {phase === "login" && (
           <Box component={"form"} className={"flex flex-col gap-4"} action={handleLogin}>
-            <Typography className={"font-bold text-2xl text-center"}>Login</Typography>
+            <Typography className={"font-bold text-2xl text-center"}>Welcome back!</Typography>
             <Box className={"flex flex-col gap-2"}>
               <TextField
                 variant={"filled"}
@@ -47,19 +59,22 @@ export default function LoginView() {
                 required
               />
             </Box>
-            <ButtonGroup className={"[&>*]:flex-1"}>
+            <Box className={"flex flex-col gap-2"}>
               <Button variant={"contained"} color={"primary"} type={"submit"}>
                 Login
               </Button>
-              <Button variant={"outlined"} color={"secondary"} type={"button"} onClick={() => setPhase("register")}>
-                Register
-              </Button>
-            </ButtonGroup>
+              <Typography className={"text-center"}>
+                {"Don't"} have an account?{" "}
+                <Link component={"button"} onClick={() => setPhase("register")}>
+                  Register now!
+                </Link>
+              </Typography>
+            </Box>
           </Box>
         )}
         {phase === "register" && (
           <Box component={"form"} className={"flex flex-col gap-4"} action={handleRegister}>
-            <Typography className={"font-bold text-2xl text-center"}>Register</Typography>
+            <Typography className={"font-bold text-2xl text-center"}>Hello there!</Typography>
             <Box className={"flex flex-col gap-2"}>
               <TextField
                 variant={"filled"}
@@ -98,14 +113,17 @@ export default function LoginView() {
                 required
               />
             </Box>
-            <ButtonGroup className={"[&>*]:flex-1"}>
+            <Box className={"flex flex-col gap-2"}>
               <Button variant={"contained"} color={"primary"} type={"submit"}>
                 Register
               </Button>
-              <Button variant={"outlined"} color={"secondary"} type={"button"} onClick={() => setPhase("login")}>
-                Login
-              </Button>
-            </ButtonGroup>
+              <Typography className={"text-center"}>
+                Have an account already?{" "}
+                <Link component={"button"} onClick={() => setPhase("login")}>
+                  Login now!
+                </Link>
+              </Typography>
+            </Box>
           </Box>
         )}
       </Paper>
